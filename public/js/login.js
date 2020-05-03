df81152f191963706b783c663fd27b66a09ec1a0
Colin Lin <colincool100@gmail.com>
2020-05-02 22:20:00 -0400

    fix login

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const loginForm = document.getElementById('login-form');
 async function loginSubmit(e){
+    e.preventDefault()
     console.log("WOW")
     $.jStorage.set("username", username.value);
     $.jStorage.set("password", password.value);
