13843e99d8b5f594eb4562171a4ce0334888d2b4
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2020-05-02 23:16:54 -0400

    logout button works
    sadasda

--- public/js/login.js
+++ public/js/login.js
@@ -2,9 +2,20 @@
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 const loginForm = document.getElementById('login-form');
+const logoutForm = document.getElementById('logout-form');
 async function loginSubmit(e){
     e.preventDefault()
     sessionStorage.setItem("username", username.value);
     sessionStorage.setItem("password", password.value);
+    location.reload();
 }
+
+async function logoutSubmit(e) {
+    e.preventDefault();
+    console.log("got here!");
+    sessionStorage.clear();
+    location.reload();
+}
+
 loginForm.addEventListener("submit", loginSubmit)
+logoutForm.addEventListener("submit", logoutSubmit)
