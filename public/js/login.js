1338be6d84747283b62b15a88d3f50d398b96b01
Colin Lin <colincool100@gmail.com>
2020-05-02 23:11:38 -0400

    login works

--- public/js/login.js
+++ public/js/login.js
@@ -6,5 +6,18 @@ async function loginSubmit(e){
     e.preventDefault()
     sessionStorage.setItem("username", username.value);
     sessionStorage.setItem("password", password.value);
+    const res = await fetch(`/api/users?name=${username.value}&password=${password.value}`)
+    const data = await res.json()
+    const data2 = await data.data
+    sessionStorage.setItem("role", data2[0].role);
+    console.log(data2[0].contactInfo.fullName)
+    if(data2[0].role == 'Courier'){
+       await sessionStorage.setItem("courier", data2[0].contactInfo.fullName);
+       window.location.href='/courier.html'
+        
+
+    }
+    else if(data2[0].role='Recipient')
+        window.location.href='/request.html'
 }
 loginForm.addEventListener("submit", loginSubmit)
