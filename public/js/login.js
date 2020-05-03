732c93fbe7e43abe83d3dc6cce8783877da9037d
Colin Lin <colincool100@gmail.com>
2020-05-02 23:43:16 -0400

    logout

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,19 @@ async function loginSubmit(e){
     e.preventDefault()
     sessionStorage.setItem("username", username.value);
     sessionStorage.setItem("password", password.value);
-    location.reload();
+    const res = await fetch(`/api/users?name=${username.value}&password=${password.value}`)
+    const data = await res.json()
+    const data2 = await data.data
+    sessionStorage.setItem("role", data2[0].role);
+    console.log(data2[0].contactInfo.fullName)
+    if(data2[0].role == 'Courier'){
+       await sessionStorage.setItem("courier", data2[0].contactInfo.fullName);
+       window.location.href='/courier.html'
+
+
+    }
+    else if(data2[0].role='Recipient')
+        window.location.href='/request.html'
 }
 
 async function logoutSubmit(e) {
